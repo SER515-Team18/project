847294f82997cbd33ca04b9d82684da5b6e4e4a3
Narendramohanm <nmuralim@asu.edu>
2019-09-25 23:51:02 -0700

    US#5-Task#34 added removal of operators from workspace on double click

--- workspace/widgets/js/index.js
+++ workspace/widgets/js/index.js
@@ -10,6 +10,8 @@ function loadworkspace (ele){
 	var button=document.createElement("BUTTON");
 	var node=document.createTextNode(ele.value);
 	button.appendChild(node);
+	button.setAttribute("id", ele.value);
+	button.setAttribute("ondblclick","remove_operator(this.id)");
 	var work=document.getElementById("workspace");
     work.appendChild(button);
 

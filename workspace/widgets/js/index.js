76085b26d3745d8066bc29ed42c938757fe7394f
Sai Vinay <saivinayg.2011@gmail.com>
2019-10-09 16:25:26 -0700

    US#8- Added drag and drop functionality for numbers in widget section

--- workspace/widgets/js/index.js
+++ workspace/widgets/js/index.js
@@ -1,18 +1,25 @@
-function addvalueToWorkSpace (id) {
-    	
-	var ele = document.getElementById(id);
-    loadworkspace(ele);
+var dropTarget = document.querySelector(".mainSection");
+var draggables = document.querySelectorAll(".col-1");
+
+
+for(let i = 0; i < draggables.length; i++) {
+  draggables[i].addEventListener("dragstart", function (ev) {
+     ev.dataTransfer.setData("srcId", ev.target.id);
+  });
 }
 
-function loadworkspace (ele){
 
-	
-	var button=document.createElement("BUTTON");
-	var node=document.createTextNode(ele.value);
-	button.appendChild(node);
-	button.setAttribute("id", ele.value);
-	button.setAttribute("ondblclick","remove_operator(this.id)");
-	var work=document.getElementById("workspace");
-    work.appendChild(button);
+dropTarget.addEventListener('dragover', function(ev) {
+  ev.preventDefault();
+});
 
-}
+dropTarget.addEventListener('drop', function(ev) {
+  ev.preventDefault();
+  let target = ev.target;
+  let droppable  = target.classList.contains('mainSection');
+  let srcId = ev.dataTransfer.getData("srcId");
+  
+  if (droppable) {
+    ev.target.appendChild(document.getElementById(srcId));
+  }
+});

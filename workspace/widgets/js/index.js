016215e45dc530bf427dfa4c811072289d73331f
Hari Krishnan Puthiya Veetil (Student) <hputhiya@asurite.asu.edu>
2019-10-14 20:35:34 -0700

    made widgets draggable

--- workspace/widgets/js/index.js
+++ workspace/widgets/js/index.js
@@ -1,25 +1,45 @@
-var dropTarget = document.querySelector(".mainSection");
-var draggables = document.querySelectorAll(".col-1");
+function addvalueToWorkSpace (id) {
+    	
+	var ele = document.getElementById(id);
+    loadworkspace(ele);
+}
+
+function loadworkspace (ele){
+
+	
+	var button=document.createElement("BUTTON");
+	var node=document.createTextNode(ele.value);
+	button.appendChild(node);
+	button.setAttribute("id", ele.value);
+	button.setAttribute("ondblclick","remove_operator(this.id)");
+	var work=document.getElementById("workspace");
+    work.appendChild(button);
+
+}
+
+var workspaceSection = document.querySelector(".mainSection");
+var widgets = document.querySelectorAll(".col-1");
 
 
-for(let i = 0; i < draggables.length; i++) {
-  draggables[i].addEventListener("dragstart", function (ev) {
-     ev.dataTransfer.setData("srcId", ev.target.id);
+for(let i = 0; i < widgets.length; i++) {
+  widgets[i].addEventListener("dragstart", function (event) {
+     event.dataTransfer.setData("srcId", event.target.id);
   });
 }
 
 
-dropTarget.addEventListener('dragover', function(ev) {
-  ev.preventDefault();
+workspaceSection.addEventListener('dragover', function(event) {
+  event.preventDefault();
 });
 
-dropTarget.addEventListener('drop', function(ev) {
-  ev.preventDefault();
-  let target = ev.target;
-  let droppable  = target.classList.contains('mainSection');
-  let srcId = ev.dataTransfer.getData("srcId");
-  
-  if (droppable) {
-    ev.target.appendChild(document.getElementById(srcId));
+workspaceSection.addEventListener('drop', function(event) {
+  event.preventDefault();
+  var target = event.target;
+  var data=event.dataTransfer.getData("srcId");
+  var copy = document.getElementById(data).cloneNode(true);
+  var canDrop  = target.classList.contains('mainSection');
+	
+  if (canDrop) {
+	loadworkspace(copy);
   }
 });

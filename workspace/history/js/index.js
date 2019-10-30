6396d2d3b74e5d0286f6139600274ac0939fa9a0
arch92 <arch92@gmail.com>
2019-10-29 21:40:10 -0700

    US#4-Task#53 Storing values in Cookies

--- workspace/history/js/index.js
+++ workspace/history/js/index.js
@@ -2,11 +2,29 @@ function fetchHistory() {
     return $("#workspace").text();
 }
 
-function loadHistory()
-{
+function loadHistory(){
     var resultStr = fetchHistory();
-    
     $("#widgetActivity").append(resultStr,"<br/>"); 
       
 }
+
+$(window).on('beforeunload', function(){    
+    var historyContent=$("#widgetActivity").text();
+    //var historyContent="hello"
+    $.cookie('HistoryContent',historyContent)
+    //console.log("unload")
+});
+
+$(window).on('load',function() {
+    if($.cookie("HistoryContent")==""){
+        $.cookie("HistoryContent",$("#widgetActivity").html());
+        
+    }   
+    else{
+        $("#widgetActivity").html($.cookie("HistoryContent"));
+    }
+     var HistoryString=$.cookie('HistoryContent')
+    $("#widgetActivity").append(HistoryString);
+    
+});
 

fb1924b1746711663a6b10b547f3e3d14c783b4e
Sai Vinay <saivinayg.2011@gmail.com>
2019-11-16 17:28:53 -0700

    US#82 Task#90 - Added clear button to clear the content from the workspace

---
+++ Login/views/workspace/result/js/index.js
@@ -0,0 +1,51 @@
+function updateResult(grade) {
+    var resultStr = fetchResultText();
+    evaluateResult(grade, resultStr);
+    
+}
+
+function fetchResultText() {
+    return $("#workspace").text();
+}
+
+function evaluateResult(grade, evalText) {
+    switch (grade) {
+        case 1,6:
+            evalGradeOne(evalText);
+            break;
+        case 9:
+            evalGradeNine(evalText);
+            break;
+
+        default:
+            break;
+    }
+}
+
+function evalGradeOne(evalText) {
+    try {
+        var result = math.evaluate(evalText);
+        $("#mathResult").html(result);
+    } catch (e) {
+        if (e instanceof SyntaxError) {
+            $("#mathResult").html(e.message);
+        }
+    }
+}
+
+function evalGradeNine(evalText) {
+    const parser = math.parser();
+    var result = null;
+    try {
+        var exprs = evalText.split("\n");
+        $.each(exprs, function (index, val) {
+            result = parser.evaluate(val);
+        });
+        
+        $("#mathResult").html(result);
+    } catch (e) {
+        if (e instanceof SyntaxError) {
+            $("#mathResult").html(e.message);
+        }
+    }
+}
